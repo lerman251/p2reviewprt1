869bc5d69f024321a1240ffd6944e57e0b509326
LThompson1s <LThompson1s@capetigers.com>
2023-08-29 14:25:05 -0500

    first commit

---
+++ js.js
@@ -0,0 +1,21 @@
+console.log("Hello, World!");
+let name = "leeman";
+console.log(name);
+const width = 6
+const length = 9
+let rectA = length * width;
+console.log(rectA);
+
+const number = 136
+if (number % 2 === 0) {
+    console.log(number + " is even");
+} else {
+    console.log(number + " is odd");
+}
+
+const Name = document.getElementById("Name");
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+const askName = prompt("Enter your name: ");
+console.log(askName);
